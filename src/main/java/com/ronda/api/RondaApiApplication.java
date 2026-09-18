5fd59e0011489a9c77b5efa8fd1bae84881cf010
IvanPlaza44 <plazaivan44@gmail.com>
2026-09-17 22:41:39 -0300

    Token

--- src/main/java/com/ronda/api/RondaApiApplication.java
+++ src/main/java/com/ronda/api/RondaApiApplication.java
@@ -1,11 +1,19 @@
 package com.ronda.api;
 
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT"
+)
 public class RondaApiApplication {
     public static void main(String[] args) {
         SpringApplication.run(RondaApiApplication.class, args);
